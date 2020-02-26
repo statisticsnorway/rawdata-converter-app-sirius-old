6c3057f7c54e4c6fd61acddbdf0ed787e26e2407
Øyvind Strømmen <insula.ventus@gmail.com>
2020-02-26 13:42:09 +0100

    Add config for the staging-bip environment

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,13 @@
 FROM adoptopenjdk/openjdk11-openj9:jdk-11.0.1.13-alpine-slim
+
 CMD mvn -B clean install -DskipTests
+
 COPY target/rawdata-converter-*.jar rawdata-converter.jar
+COPY target/classes/logback.xml /conf
+COPY target/classes/logback-bip.xml /conf
+
 VOLUME ["/conf", "/run/ssb/secrets"]
+
 EXPOSE 8080
-CMD java -Dcom.sun.management.jmxremote -noverify ${JAVA_OPTS} -Dmicronaut.config.files="/conf/application.properties" -jar rawdata-converter.jar
+
+CMD java -Dcom.sun.management.jmxremote -noverify ${JAVA_OPTS} -jar rawdata-converter.jar
--- src/main/java/no/ssb/rawdata/converter/app/sirius/Application.java
+++ src/main/java/no/ssb/rawdata/converter/app/sirius/Application.java
@@ -1,10 +1,14 @@
 package no.ssb.rawdata.converter.app.sirius;
 
 import io.micronaut.runtime.Micronaut;
+import no.ssb.rawdata.converter.core.DefaultConverterApplication;
+import no.ssb.rawdata.converter.core.util.EnvrionmentVariables;
 
-public class Application {
-
+public class Application extends DefaultConverterApplication {
     public static void main(String[] args) {
-        Micronaut.run(Application.class);
+        Micronaut.build(null)
+                .mainClass(Application.class)
+                .environmentVariableIncludes(EnvrionmentVariables.withPrefix("RAWDATA_CLIENT").toArray(new String[0]))
+                .start();
     }
 }
